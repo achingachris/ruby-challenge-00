4673305576b536df7dcbfc90ab22d079d6a9d254
itschrisagain <chris.achinga@student.moringaschool.com>
2022-11-07 03:53:32 +0300

    test

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,41 @@
-class Magazine
-  attr_accessor :name, :category
+# class Magazine
+#   attr_accessor :name, :category
 
 
-  def initialize(name, category)
-    @name = name
-    @category = category
+#   def initialize(name, category)
+#     @name = name
+#     @category = category
 
+#   end
+
+
+# end
+
+
+class Magazine < ActiveRecord::Base
+  has_many :subscriptions
+  has_many :readers, through: :subscriptions
+
+
+
+  def email_list
+      tronch = ""
+      self.readers.all.map{|reader| print "#{tronch + reader.email};"}
   end
 
+  def self.most_popular
+      array = []
+      length = 0
+      chosen_one = []
 
+      self.all.map{|mag| 
+      if (mag.subscriptions.length > length)
+          length = mag.subscriptions.length
+          chosen_one = []
+          chosen_one << mag
+
+      end
+  }
+      puts chosen_one[0].title
+  end
 end
